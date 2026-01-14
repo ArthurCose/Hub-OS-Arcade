a838a9e8f41b6252d6fc009a09595be1f42adc2e
Arthur Cosentino <arthurcose@gmail.com>
2026-01-14 18:04:06 -0500

    Delay tile state changes to override stage augments

--- mods/arcade_fields/entry.lua
+++ mods/arcade_fields/entry.lua
@@ -13,7 +13,40 @@ function Lib.randomize_ambience(encounter)
 end
 
 function Lib.randomize_field()
+  -- delay most tile state changes to prevent overwrite from stage augments
+  ---@type [number, number, TileState][]
+  local pending_tile_state_changes = {}
+
+  local state_delay = 5
+  local artifact = Artifact.new()
+  artifact:create_component(Lifetime.Scene).on_update_func = function()
+    state_delay = state_delay - 1
+
+    if state_delay > 0 then
+      return
+    end
+
+    artifact:delete()
+
+    for _, change in ipairs(pending_tile_state_changes) do
+      local x, y, state = table.unpack(change)
+      local tile = Field.tile_at(x, y)
+
+      if tile then
+        tile:set_state(state)
+      end
+    end
+  end
+
+  Field.spawn(artifact, 0, 0)
+
+  -- helper functions
   local function set_state(x, y, state)
+    if state ~= TileState.PermaHole and state ~= TileState.Cracked then
+      pending_tile_state_changes[#pending_tile_state_changes + 1] = { x, y, state }
+      return
+    end
+
     local tile = Field.tile_at(x, y)
 
     if tile then
