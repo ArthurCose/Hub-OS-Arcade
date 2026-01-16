9d7b63bbfdba10b9b5bae3436b728d9a27519dfd
Arthur Cosentino <arthurcose@gmail.com>
2026-01-16 18:36:39 -0500

    Add restrictions

--- scripts/arcade/main.lua
+++ scripts/arcade/main.lua
@@ -1,2 +1,6 @@
 require("scripts/arcade/arenas")
 require("scripts/arcade/prizes")
+
+Net:on("player_connect", function(event)
+  Net.set_player_restrictions(event.player_id, "/server/assets/restrictions.toml")
+end)
