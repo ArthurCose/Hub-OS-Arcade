1e7095e2aac5991ee3813a45c12672d60d631f72
Arthur Cosentino <arthurcose@gmail.com>
2026-06-30 15:32:18 -0400

    Add limited time freeze mats

---
+++ assets/tiles/limited_time_freeze.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="limited_time_freeze" tilewidth="24" tileheight="32" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="9"/>
+ <image source="limited_time_freeze.png" width="24" height="32"/>
+</tileset>
