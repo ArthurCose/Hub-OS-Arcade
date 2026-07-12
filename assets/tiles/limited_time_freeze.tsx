09c8bc1fb50fd688785f6f0cdd21152264559623
Arthur Cosentino <arthurcose@gmail.com>
2026-07-12 17:58:35 -0400

    Update icon for limited time freeze mats

--- assets/tiles/limited_time_freeze.tsx
+++ assets/tiles/limited_time_freeze.tsx
@@ -1,5 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="limited_time_freeze" tilewidth="24" tileheight="32" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="9"/>
- <image source="limited_time_freeze.png" width="24" height="32"/>
+<tileset version="1.10" tiledversion="1.12.2" name="limited_time_freeze" tilewidth="32" tileheight="32" tilecount="18" columns="4" objectalignment="bottom">
+ <grid orientation="orthogonal" width="24" height="32"/>
+ <image source="limited_time_freeze.png" width="128" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+  </animation>
+ </tile>
 </tileset>
