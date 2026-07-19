4c85d6017bc5c749b2eb4a0de800160ce36957b8
Arthur Cosentino <arthurcose@gmail.com>
2026-07-19 01:43:06 -0400

    Allow 0 second timers again based on feedback

--- scripts/arcade/arena_console.lua
+++ scripts/arcade/arena_console.lua
@@ -81,7 +81,7 @@ local config_options = {
       return "Select Time", value .. "s"
     end,
     on_select = function(player_id, _, callback)
-      prompt_range(player_id, 1, 10000, callback)
+      prompt_range(player_id, 0, 10000, callback)
     end
   },
   {
